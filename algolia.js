a6765413de39c2ad2f7627e7063ee6a6c88b4743
Mohan <100554588+mohan-chaudhari@users.noreply.github.com>
2022-04-20 18:32:59 +0530

    init commit

---
+++ algolia.js
@@ -0,0 +1,29 @@
+const algoliasearch = require("algoliasearch");
+
+const client = algoliasearch("I0PX4WIMQC", "af933b712260b02bc760b7ba8af7434b");
+const index = client.initIndex("new-index-1650449183");
+
+// const objects = [
+//   {
+//     objectID: 1,
+//     name: "Foo",
+//   },
+// ];
+
+// index
+//   .saveObjects(objects)
+//   .then(({ objectIDs }) => {
+//     console.log(objectIDs);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+index
+  .search("Tyler")
+  .then(({ hits }) => {
+    console.log(hits);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
